a64e9d4daa442301f7bbac93908946cb27c9380b
Kostya <kostyukovkg@yahoo.com>
2023-04-30 18:35:22 +0300

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+#st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Konstantin Kostyukov")
+    content = """
+    Write smth
+    """
+    st.write(content)
+
