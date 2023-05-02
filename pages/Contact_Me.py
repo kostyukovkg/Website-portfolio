7ad0191fecaa77f0697062903a0088923edb08a7
Kostya <kostyukovkg@yahoo.com>
2023-05-02 14:44:48 +0300

    Day 23: Send email

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header('Contact Me')
+
+with st.form(key="main_form"):
+    user_email = st.text_input("Enter your email adress")
+    message = st.text_area("Enter your message to me here")  # Allows to enter a multiple line message
+    button = st.form_submit_button()  # special button that submits text for the form.
+        # text can be sent to a database or by email etc.
+    
